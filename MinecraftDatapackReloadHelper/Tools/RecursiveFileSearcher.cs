9cd65d11ba6d63e65a9ae2693c636ea8c6e803b7
Kyuri <107470858+Kyuri-jp@users.noreply.github.com>
2024-07-06 07:35:00 +0900

    抑制

--- MinecraftDatapackReloadHelper/Tools/RecursiveFileSearcher.cs
+++ MinecraftDatapackReloadHelper/Tools/RecursiveFileSearcher.cs
@@ -22,10 +22,14 @@
                 return;
             }
 
+#pragma warning disable CS8600 // Null リテラルまたは Null の可能性がある値を Null 非許容型に変換しています。
             DirectoryInfo directoryInfo = Directory.GetParent(begin);
+#pragma warning restore CS8600 // Null リテラルまたは Null の可能性がある値を Null 非許容型に変換しています。
             try
             {
+#pragma warning disable CS8602 // null 参照の可能性があるものの逆参照です。
                 begin = directoryInfo.FullName;
+#pragma warning restore CS8602 // null 参照の可能性があるものの逆参照です。
             }
             catch (NullReferenceException)
             {
