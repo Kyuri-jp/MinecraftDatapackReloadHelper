9500792098c8309649b67ede95cdc125f43fba0c
Kyuri <107470858+Kyuri-jp@users.noreply.github.com>
2024-07-19 12:01:01 +0900

    appsettingにautoの引数を追加

--- MinecraftDatapackReloadHelper/Tools/Control/Setting/ApplicationSetting.cs
+++ MinecraftDatapackReloadHelper/Tools/Control/Setting/ApplicationSetting.cs
@@ -1,5 +1,7 @@
 ﻿using System.Net;
+using System.Net.NetworkInformation;
 using System.Net.Sockets;
+using CoreRCON;
 using MinecraftDatapackReloadHelper.Tools.Minecraft;
 
 namespace MinecraftDatapackReloadHelper.Tools.Control.Setting
@@ -14,14 +16,8 @@ namespace MinecraftDatapackReloadHelper.Tools.Control.Setting
                 Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine("Inputed localhost.\nset this computer's private ip adress.");
                 Console.ForegroundColor = ConsoleColor.White;
-                foreach (IPAddress ip in Dns.GetHostAddresses(Dns.GetHostName()))
-                {
-                    if (ip.AddressFamily.Equals(AddressFamily.InterNetwork))
-                    {
-                        rconIP = ip.ToString();
-                        break;
-                    }
-                }
+
+                rconIP = Getv4Adress();
             }
 
             string? rconPort = Asker("Please enter rcon port.");
@@ -43,6 +39,9 @@ namespace MinecraftDatapackReloadHelper.Tools.Control.Setting
 
         private static string Asker(string message, bool mayNull = false)
         {
+            ArgumentException.ThrowIfNullOrWhiteSpace(message);
+            ArgumentException.ThrowIfNullOrEmpty(message);
+
             string? reader = string.Empty;
 
             while (reader == string.Empty)
@@ -66,9 +65,68 @@ namespace MinecraftDatapackReloadHelper.Tools.Control.Setting
             return reader;
         }
 
+        private static string Getv4Adress()
+        {
+            string? v4 = null;
+            foreach (IPAddress ip in Dns.GetHostAddresses(Dns.GetHostName()))
+            {
+                if (ip.AddressFamily.Equals(AddressFamily.InterNetwork))
+                {
+                    v4 = ip.ToString();
+                    break;
+                }
+            }
+            ArgumentException.ThrowIfNullOrEmpty(v4);
+            return v4;
+        }
+
         internal static async Task AutoChangeRconSettingAsync()
         {
-            ServerProperties.Parse(Settings.Client_Copy);
+            DirectoryInfo copyDirectoryInfo = new(Settings.Client_Copy); ;
+
+#pragma warning disable CS8602 // null 参照の可能性があるものの逆参照です。
+            string filePath = Path.Combine(copyDirectoryInfo.Parent.Parent.FullName, "server.properties");
+#pragma warning restore CS8602 // null 参照の可能性があるものの逆参照です。
+
+            if (!File.Exists(filePath))
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Copy path is null or empty.");
+                Console.ForegroundColor = ConsoleColor.White;
+
+                while (true)
+                {
+                    string copy = Asker("Please enter copy path.");
+                    if (!Directory.Exists(copy))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine($"{copy} is not exists.");
+                        continue;
+                    }
+                    RecursiveFileSearcher recursiveFileSearcher = new();
+
+                    if (!recursiveFileSearcher.RecursiveFileExists(copy, "level.dat"))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine($"Not found level file in {copy}'s parents");
+                        continue;
+                    }
+                    if (!recursiveFileSearcher.RecursiveFileExists(copy, "server.properties"))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine($"Not found server.properties in {copy}'s parents.\nMaybe this directory is not server.");
+                        continue;
+                    }
+
+                    break;
+                }
+            }
+
+            Dictionary<string, string> PropertyData = ServerProperties.Parse(filePath);
+
+            Settings.Rcon_IP = Getv4Adress();
+            Settings.Rcon_Port = ushort.Parse(PropertyData["rcon.port"]);
+            Settings.Rcon_Password = PropertyData["rcon.password"];
 
             await ConnectionTest.ConnectingTesterAsync();
         }
