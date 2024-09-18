2cdb3648c8cbc6345160f049cd8480d81ed35a0b
Kyuri <107470858+Kyuri-jp@users.noreply.github.com>
2024-09-18 22:38:47 +0900

    help --more=[<command>]で指定したコマンドの詳細を表示するように

--- MinecraftDatapackReloadHelper/Systems/Commands/Help.cs
+++ MinecraftDatapackReloadHelper/Systems/Commands/Help.cs
@@ -1,20 +1,55 @@
 ﻿using MinecraftDatapackReloadHelper.Interfaces.Commands;
+using MinecraftDatapackReloadHelper.Systems.Control;
+using MinecraftDatapackReloadHelper.Tools;
 
 namespace MinecraftDatapackReloadHelper.Systems.Commands
 {
-    internal class Help : IToolCommand
+    internal class Help : IToolCommand, IHasArgsCommand
     {
+        private enum Args
+        {
+            More
+        }
+
+        private readonly Dictionary<string, string[]> argsData = new()
+        {
+            {Args.More.ToString(),["指定したコマンドの詳細を表示します",$"--{Args.More}=[<Command>]"]}
+        };
+
         public Task Run(Dictionary<string, List<string>> args)
         {
-            Console.WriteLine("コマンドや引数は大文字小文字の区別はありません\n" +
-                        "また、引数は -- で区切ります\n" +
-                        "- は区切りとして認識されません\n" +
-                        "引数についてはReadmeを参照ください -> https://github.com/Kyuri-jp/MinecraftDatapackReloadHelper\n");
+            if (args.ContainsKey(Args.More.ToString()))
+            {
+                if (!CommandSelector.GetCommandInst().ContainsKey(StringUtl.ToUpperOnlyFirstLetter(args[Args.More.ToString()][0])))
+                {
+                    Console.WriteLine($"{args[Args.More.ToString()][0]} was not found.");
+                    return Task.CompletedTask;
+                }
 
-            foreach (KeyValuePair<string, string> keyValuePair in Control.CommandSelector.GetCommandHelp())
-                Console.WriteLine($"{keyValuePair.Key} : {keyValuePair.Value}");
+                if (!CommandSelector.GetCommandInst()[StringUtl.ToUpperOnlyFirstLetter(args[Args.More.ToString()][0])].GetType().GetInterfaces().Contains(typeof(IHasArgsCommand)))
+                {
+                    Console.WriteLine($"Args of {args.ElementAt(0).Key} was not found.");
+                    return Task.CompletedTask;
+                }
+                IHasArgsCommand command = (IHasArgsCommand)CommandSelector.GetCommandInst()[StringUtl.ToUpperOnlyFirstLetter(args[Args.More.ToString()][0])];
+                Console.WriteLine($"[{args.ElementAt(0).Key}]-> {CommandSelector.GetCommandHelp()[StringUtl.ToUpperOnlyFirstLetter(args[Args.More.ToString()][0])]}");
+                foreach (KeyValuePair<string, string[]> keyValuePair in command.GetArgs())
+                    Console.WriteLine($"{keyValuePair.Key} : {string.Join(" / ", keyValuePair.Value)}");
+            }
+            else
+            {
+                Console.WriteLine("コマンドや引数は大文字小文字の区別はありません\n" +
+                            "また、引数は -- で区切ります\n" +
+                            "- は区切りとして認識されません\n" +
+                            "引数についてはReadmeを参照ください -> https://github.com/Kyuri-jp/MinecraftDatapackReloadHelper\n" +
+                            "help --more=[<command>] を使用するとより詳しい情報を見ることができます");
 
+                foreach (KeyValuePair<string, string> keyValuePair in CommandSelector.GetCommandHelp())
+                    Console.WriteLine($"{keyValuePair.Key} : {keyValuePair.Value}");
+            }
             return Task.CompletedTask;
         }
+
+        public Dictionary<string, string[]> GetArgs() => argsData;
     }
 }
--- MinecraftDatapackReloadHelper/Systems/Commands/Upload.cs
+++ MinecraftDatapackReloadHelper/Systems/Commands/Upload.cs
@@ -3,7 +3,7 @@ using MinecraftDatapackReloadHelper.Tools;
 
 namespace MinecraftDatapackReloadHelper.Systems.Commands
 {
-    internal class Upload : IToolCommand
+    internal class Upload : IToolCommand, IHasArgsCommand
     {
         private static readonly DirectoryInfo? copy = Directory.GetParent(Settings.Client_Copy);
 
@@ -13,6 +13,14 @@ namespace MinecraftDatapackReloadHelper.Systems.Commands
             Custompath,
             Nonclean,
             Notopen,
+        };
+
+        private readonly Dictionary<string, string[]> argsData = new()
+        {
+            {Args.Additional.ToString(),["生成したZipファイルに継ぎ足しで文字を加えます","--additional=[<string>]"] },
+            {Args.Custompath.ToString(),["対象となるフォルダを変更します","--custompath=[<severdirectory>]"] },
+            {Args.Nonclean.ToString(),["advancementフォルダなどの削除を無効化します","--nonclean"] },
+            {Args.Notopen.ToString(),["圧縮し終えた後のフォルダ表示を無効化します","--notopen"] }
         };
 
 #pragma warning disable CS8602 // null 参照の可能性があるものの逆参照です。
@@ -35,5 +43,7 @@ namespace MinecraftDatapackReloadHelper.Systems.Commands
             }
             await WorldUpload.Upload(source, Settings.Client_UploadOutput, !args.ContainsKey(Args.Nonclean.ToString()), !args.ContainsKey(Args.Notopen.ToString()), additional);
         }
+
+        public Dictionary<string, string[]> GetArgs() => argsData;
     }
 }
