b2181e2f94f0162bcb01333d41f1ada105481bfd
Kyuri <107470858+Kyuri-jp@users.noreply.github.com>
2024-09-21 20:12:51 +0900

    とりあえず形だけ

--- MinecraftDatapackReloadHelper/Systems/Commands/Upload.cs
+++ MinecraftDatapackReloadHelper/Systems/Commands/Upload.cs
@@ -1,16 +1,19 @@
 ﻿using MinecraftDatapackReloadHelper.Interfaces.Commands;
+using MinecraftDatapackReloadHelper.Libs.Files;
+using MinecraftDatapackReloadHelper.Libs.Files.Directories;
+using MinecraftDatapackReloadHelper.Tools.Display;
 using MinecraftDatapackReloadHelper.Tools;
+using System.IO.Compression;
 
 namespace MinecraftDatapackReloadHelper.Systems.Commands
 {
     internal class Upload : IToolCommand, IHasArgsCommand
     {
-        private static readonly DirectoryInfo? copy = Directory.GetParent(Settings.Client_Copy);
-
         private enum Args
         {
             Additional,
             Custompath,
+            Extractdatapack,
             Nonclean,
             Notopen,
         };
@@ -19,29 +22,64 @@ namespace MinecraftDatapackReloadHelper.Systems.Commands
         {
             {Args.Additional.ToString(),["生成したZipファイルに継ぎ足しで文字を加えます","--additional=[<string>]"] },
             {Args.Custompath.ToString(),["対象となるフォルダを変更します","--custompath=[<severdirectory>]"] },
+            {Args.Extractdatapack.ToString(),["データパックのみを圧縮します", "--extractdatapack (--additional=[<string>]) (--custompath=[<directory>])"] },
             {Args.Nonclean.ToString(),["advancementフォルダなどの削除を無効化します","--nonclean"] },
             {Args.Notopen.ToString(),["圧縮し終えた後のフォルダ表示を無効化します","--notopen"] }
         };
 
-#pragma warning disable CS8602 // null 参照の可能性があるものの逆参照です。
-        private string source = copy.FullName;
-#pragma warning restore CS8602 // null 参照の可能性があるものの逆参照です。
-
         public async Task Run(Dictionary<string, List<string>> args)
         {
-            string additional = string.Empty;
-            if (args.ContainsKey(Args.Additional.ToString()))
-                additional = args[Args.Additional.ToString()][0];
-
-            if (args.ContainsKey(Args.Custompath.ToString()))
+            if (args.ContainsKey(Args.Extractdatapack.ToString()))
             {
-                source = args[Args.Custompath.ToString()][0];
-                if (!Directory.Exists(source))
-                    throw new DirectoryNotFoundException(source);
-                if (!Directory.Exists(Path.Combine(source, "level.dat")))
-                    throw new FileNotFoundException(Path.Combine(source, "level.dat"));
+                string source = RecursiveFileSearcher.RecursiveGetDirectoryPath(Settings.Client_Copy, "pack.mcmeta");
+                string additional = string.Empty;
+                if (args.ContainsKey(Args.Additional.ToString()))
+                    additional = args[Args.Additional.ToString()][0];
+                if (args.ContainsKey(Args.Custompath.ToString()))
+                {
+                    source = args[Args.Custompath.ToString()][0];
+                    if (!Directory.Exists(source))
+                        throw new DirectoryNotFoundException(source);
+                    if (!Directory.Exists(Path.Combine(RecursiveFileSearcher.RecursiveGetDirectoryPath(source, "pack.mcmeta"), "level.dat")))
+                        throw new FileNotFoundException(Path.Combine(source, "pack.mcmeta"));
+                }
+                string fileName = source;
+                if (RecursiveFileSearcher.RecursiveFileExists(source, "server.properties"))
+                    fileName = RecursiveFileSearcher.RecursiveGetDirectoryPath(source, "server.properties");
+
+                string output = Path.Combine(Settings.Client_UploadOutput, Path.GetDirectoryName(fileName)!) + $"{additional}";
+
+                if (File.Exists(output + ".zip"))
+                {
+                    Console.WriteLine("Set Index");
+                    int i = 1;
+                    while (File.Exists(output + $"({i}).zip"))
+                        i++;
+
+                    output += $"({i})";
+                }
+
+                ZipFile.CreateFromDirectory(source, output + ".zip");
             }
-            await WorldUpload.Upload(source, Settings.Client_UploadOutput, !args.ContainsKey(Args.Nonclean.ToString()), !args.ContainsKey(Args.Notopen.ToString()), additional);
+            else
+            {
+                DirectoryInfo copy = Directory.GetParent(Settings.Client_Copy)!;
+                string source = copy.FullName;
+
+                string additional = string.Empty;
+                if (args.ContainsKey(Args.Additional.ToString()))
+                    additional = args[Args.Additional.ToString()][0];
+
+                if (args.ContainsKey(Args.Custompath.ToString()))
+                {
+                    source = args[Args.Custompath.ToString()][0];
+                    if (!Directory.Exists(source))
+                        throw new DirectoryNotFoundException(source);
+                    if (!Directory.Exists(Path.Combine(source, "level.dat")))
+                        throw new FileNotFoundException(Path.Combine(source, "level.dat"));
+                }
+                await WorldUpload.Upload(source, Settings.Client_UploadOutput, !args.ContainsKey(Args.Nonclean.ToString()), !args.ContainsKey(Args.Notopen.ToString()), additional);
+            }
         }
 
         public Dictionary<string, string[]> GetArgs() => argsData;
