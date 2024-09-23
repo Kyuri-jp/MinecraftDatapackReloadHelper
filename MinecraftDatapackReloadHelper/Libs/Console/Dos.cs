651d0175d6512ffc717fc8ef58e82c0f9ca9af2e
Kyuri <107470858+Kyuri-jp@users.noreply.github.com>
2024-09-23 11:35:53 +0900

    コマンドの実行結果でmajor versionを取得

---
+++ MinecraftDatapackReloadHelper/Libs/Console/Dos.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+namespace MinecraftDatapackReloadHelper.Libs.Console
+{
+    internal class Dos
+    {
+        //ref https://resanaplaza.com/2021/05/03/%e3%80%90%e3%82%b3%e3%83%94%e3%83%9a%e3%81%a7%e5%ae%8c%e4%ba%86%e3%80%91c%e3%81%a7dos-%e3%82%b3%e3%83%9e%e3%83%b3%e3%83%89%e3%82%92%e5%ae%9f%e8%a1%8c%e3%81%97%e7%b5%90%e6%9e%9c%e3%82%92%e5%8f%96/
+        public static IEnumerable<string> RunCommand(string command)
+        {
+            ProcessStartInfo psInfo = new ProcessStartInfo
+            {
+                FileName = "cmd", // 実行するファイル
+                Arguments = "/c " + command,//引数
+                CreateNoWindow = true, // コンソール・ウィンドウを開かない
+                UseShellExecute = false, // シェル機能を使用しない
+                RedirectStandardOutput = true // 標準出力をリダイレクト
+            };
+
+            Process p = Process.Start(psInfo)!; // アプリの実行開始
+
+            while (p.StandardOutput.ReadLine() is { } line)
+            {
+                System.Console.WriteLine(line);
+                yield return line;
+            }
+        }
+    }
+}
--- MinecraftDatapackReloadHelper/Libs/Java/Java.cs
+++ MinecraftDatapackReloadHelper/Libs/Java/Java.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using MinecraftDatapackReloadHelper.Libs.Console;
 
 namespace MinecraftDatapackReloadHelper.Libs.Java
 {
@@ -10,8 +11,28 @@ namespace MinecraftDatapackReloadHelper.Libs.Java
 
         internal void RunJarFile(string file, string arg = "")
         {
+            GetJarMajorVersion(file);
             Process process = Process.Start(Path.Combine(_bin, "javaw.exe"), $"-jar {file} {arg}");
             process.WaitForExitAsync();
+        }
+
+        internal int GetJarMajorVersion(string file)
+        {
+            string currentDir = Directory.GetCurrentDirectory();
+            string tempFolder = Path.Combine(Path.GetTempPath(), new DirectoryInfo(file).Name);
+            if (Directory.Exists(tempFolder))
+                Directory.Delete(tempFolder, true);
+            Directory.CreateDirectory(tempFolder);
+            Directory.SetCurrentDirectory(tempFolder);
+            Process.Start(Path.Combine(_bin, "jar.exe"), $"xf {file}").WaitForExit();
+            Directory.SetCurrentDirectory(currentDir);
+            string[] classFiles = Directory.GetFiles(tempFolder, "*.*", SearchOption.AllDirectories).Where(c => ".class".Any(c.EndsWith)).ToArray();
+            List<int> foundClassVersions = [];
+            foundClassVersions.AddRange(from classFile in classFiles
+                                        from result in Dos.RunCommand($"{Path.Combine(_bin, "javap.exe")} -v {classFile}")
+                                        where result.Contains("major version", StringComparison.OrdinalIgnoreCase)
+                                        select int.Parse(result.Substring(result.IndexOf(':') + 1, result.Length)));
+            return foundClassVersions.Max();
         }
     }
 }
