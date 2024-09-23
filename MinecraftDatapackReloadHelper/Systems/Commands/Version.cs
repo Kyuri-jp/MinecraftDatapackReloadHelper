36c93f4562809d032c1dd7d2991bd0e7e9358ff4
Kyuri <107470858+Kyuri-jp@users.noreply.github.com>
2024-09-23 15:25:12 +0900

    versionの引数情報忘れ

--- MinecraftDatapackReloadHelper/Systems/Commands/Version.cs
+++ MinecraftDatapackReloadHelper/Systems/Commands/Version.cs
@@ -3,12 +3,17 @@ using MinecraftDatapackReloadHelper.Tools;
 
 namespace MinecraftDatapackReloadHelper.Systems.Commands
 {
-    internal class Version : IToolCommand
+    internal class Version : IToolCommand, IHasArgsCommand
     {
         private enum Args
         {
             Updatecheck
         }
+
+        private readonly Dictionary<string, string[]> _argsData = new()
+        {
+            {Args.Updatecheck.ToString(),["最新のリリースタグを確認します",$"--{Args.Updatecheck}"]}
+        };
 
         public async Task Run(Dictionary<string, List<string>> args)
         {
@@ -16,5 +21,7 @@ namespace MinecraftDatapackReloadHelper.Systems.Commands
             if (args.ContainsKey(Args.Updatecheck.ToString()))
                 await UpdateCheck.UpdateCheckerAsync();
         }
+
+        Dictionary<string, string[]> IHasArgsCommand.GetArgs() => _argsData;
     }
 }
