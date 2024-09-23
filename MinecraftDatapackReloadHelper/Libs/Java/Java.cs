33f4650812887f65929a1739a92055b5c594fe36
Kyuri <107470858+Kyuri-jp@users.noreply.github.com>
2024-09-23 10:19:10 +0900

    _

--- MinecraftDatapackReloadHelper/Libs/Java/Java.cs
+++ MinecraftDatapackReloadHelper/Libs/Java/Java.cs
@@ -4,13 +4,13 @@ namespace MinecraftDatapackReloadHelper.Libs.Java
 {
     internal class Java
     {
-        private readonly string bin;
+        private readonly string _bin;
 
-        internal Java(string bin) => this.bin = bin;
+        internal Java(string bin) => _bin = bin;
 
         internal void RunJarFile(string file, string arg = "")
         {
-            Process process = Process.Start(Path.Combine(bin, "java.exe"), $"-jar {file} {arg}");
+            Process process = Process.Start(Path.Combine(_bin, "java.exe"), $"-jar {file} {arg}");
             process.WaitForExitAsync();
         }
     }
--- MinecraftDatapackReloadHelper/Libs/Java/SearchJava.cs
+++ MinecraftDatapackReloadHelper/Libs/Java/SearchJava.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MinecraftDatapackReloadHelper.Libs.Java
+﻿namespace MinecraftDatapackReloadHelper.Libs.Java
 {
     internal class SearchJava
     {
