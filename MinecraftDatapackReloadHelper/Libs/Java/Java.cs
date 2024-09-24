75395afc60d53ceb397ac1f6bbd6bfa98d444b39
Kyuri <107470858+Kyuri-jp@users.noreply.github.com>
2024-09-24 20:39:06 +0900

    Javaディレクトリを軽量に取得

--- MinecraftDatapackReloadHelper/Libs/Java/Java.cs
+++ MinecraftDatapackReloadHelper/Libs/Java/Java.cs
@@ -1,5 +1,5 @@
-﻿using System.Diagnostics;
-using MinecraftDatapackReloadHelper.Libs.Console;
+﻿using MinecraftDatapackReloadHelper.Libs.Console;
+using System.Diagnostics;
 
 namespace MinecraftDatapackReloadHelper.Libs.Java
 {
@@ -11,6 +11,8 @@ namespace MinecraftDatapackReloadHelper.Libs.Java
 
         internal void RunJarFile(string file, string arg = "")
         {
+            foreach (KeyValuePair<string, string> keyValuePair in GetJavas())
+                System.Console.WriteLine($"{keyValuePair.Key} : {keyValuePair.Value}");
             System.Console.WriteLine(GetJarMajorVersion(file));
             Process process = Process.Start(Path.Combine(_bin, "javaw.exe"), $"-jar {file} {arg}");
             process.WaitForExitAsync();
@@ -28,8 +30,40 @@ namespace MinecraftDatapackReloadHelper.Libs.Java
             Directory.SetCurrentDirectory(currentDir);
             string[] classFiles = Directory.GetFiles(tempFolder, "*.class", SearchOption.AllDirectories);
             List<int> foundClassVersions = [];
-            foundClassVersions.AddRange(from classFile in classFiles from result in Dos.RunCommand($"\"{Path.Combine(_bin, "javap.exe")}\" -v {classFile}") where result.Contains("major version", StringComparison.OrdinalIgnoreCase) select int.Parse(result.Split(':')[1]));
+            foundClassVersions.AddRange(from classFile in classFiles
+                                        from result in Dos.RunCommand($"\"{Path.Combine(_bin, "javap.exe")}\" -v {classFile}")
+                                        where result.Contains("major version", StringComparison.OrdinalIgnoreCase)
+                                        select int.Parse(result.Split(':')[1]));
             return foundClassVersions.Max();
+        }
+
+        internal static Dictionary<string, string> GetJavas()
+        {
+            Dictionary<string, string> foundJavaDirectory = [];
+            foreach (string exeFile in Directory.GetFiles(
+                         Directory.GetParent(Environment.GetEnvironmentVariable("JAVA_HOME")!)!.FullName,
+                         "java.exe",
+                         SearchOption.AllDirectories))
+            {
+                if (ReleaseParse(Directory.GetParent(exeFile)!.Parent!.FullName).TryGetValue("JAVA_VERSION", out string? value))
+                    foundJavaDirectory.Add(value, Directory.GetParent(exeFile)!.FullName);
+            }
+
+            return foundJavaDirectory;
+        }
+
+        internal static Dictionary<string, string> ReleaseParse(string javaFolder)
+        {
+            Dictionary<string, string> releaseInfo = [];
+            if (!File.Exists(Path.Combine(javaFolder, "release")))
+                return releaseInfo;
+            foreach (var item in File.ReadAllLines(Path.Combine(javaFolder, "release")))
+            {
+                if (item[0] == '#') continue;
+                releaseInfo.Add(item[..item.IndexOf('=')], item[(item.IndexOf('=') + 1)..].Replace('"', ' '));
+            }
+
+            return releaseInfo;
         }
     }
 }
