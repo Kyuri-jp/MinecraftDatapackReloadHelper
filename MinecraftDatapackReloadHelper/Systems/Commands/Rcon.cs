75bc1076d8dda48eceaa1b34b2897fa1d9aa792c
Kyuri <107470858+Kyuri-jp@users.noreply.github.com>
2024-10-09 07:51:46 +0900

    形だけ

---
+++ MinecraftDatapackReloadHelper/Systems/Commands/Rcon.cs
@@ -0,0 +1,25 @@
+﻿using MinecraftDatapackReloadHelper.Abstract.Commands;
+using MinecraftDatapackReloadHelper.Interfaces.Commands;
+
+namespace MinecraftDatapackReloadHelper.Systems.Commands
+{
+    internal class Rcon : Command, IArgsable
+    {
+        private enum Args
+        {
+            GeneratePassword
+        }
+
+        private readonly Dictionary<string, string[]> _argsData = new()
+        {
+            { Args.GeneratePassword.ToString(), ["パスワードをランダムに設定します+", "--generatepassword"] }
+        };
+
+        internal override Task Run(Dictionary<string, List<string>> args)
+        {
+            throw new NotImplementedException();
+        }
+
+        Dictionary<string, string[]> IArgsable.GetArgs() => _argsData;
+    }
+}
